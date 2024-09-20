cab27f77996f5bbd37c281ca50a46c21d9e7a5e0
Muhammad Banhawy <devbenho@gmail.com>
2024-09-20 21:17:16 +0300

    FIXED: Handling ObjectID validation

--- server/src/application/auth/register/register.use-case.ts
+++ server/src/application/auth/register/register.use-case.ts
@@ -8,6 +8,10 @@ import { JwtPayload } from '@contracts/services/IJwt';
 import { HasherDomainService } from '@domain/shared/services';
 import { UserModel } from '@infrastructure/users/user.model';
 import {ScopeModel} from "@infrastructure/scopes/scope.model";
+import { BadRequestException, ResourceNotFoundException } from '@web/rest/expections';
+import { Logger } from '@domain/shared';
+import { Types } from 'mongoose';
+import { InvalidParameterException } from '@domain/shared/exceptions';
 
 @UseCase()
 class RegisterUsecase extends BaseUseCase<CreateUserDto, AuthResponseDto> {
@@ -35,6 +39,20 @@ class RegisterUsecase extends BaseUseCase<CreateUserDto, AuthResponseDto> {
   }
 
   private async createUser(request: CreateUserDto): Promise<User> {
+    if (!Types.ObjectId.isValid(request.scopeId)) {
+      throw new InvalidParameterException('Invalid ID');
+    }
+
+    const scope = await ScopeModel.findById(request.scopeId);
+    if (!scope) {
+      throw new ResourceNotFoundException('Scope not found');
+    }
+
+    // CHECK if the user already exists
+    const userExist = await UserModel.findOne({ email: request.email });
+    if (userExist) {
+      throw new BadRequestException('User already exists');
+    }
     const user = User.create(
       null,
       request.email,
@@ -44,16 +62,19 @@ class RegisterUsecase extends BaseUseCase<CreateUserDto, AuthResponseDto> {
       new Date(),
       request.triggeredBy.who,
     );
+
     user.password = await HasherDomainService.hash(user.password);
     const createdUser = await UserModel.create(user);
     await createdUser.save();
+    user.id = createdUser._id.toString();
     return user
   }
 
   private async validateScope(scopeId: string): Promise<any> {
     const scopeName = await ScopeModel.findById(scopeId);
     if (!scopeName) {
-      throw new Error('Scope not found');
+      Logger.error('Scope not found');
+      throw new ResourceNotFoundException('Scope not found');
     }
     return scopeName;
   }
